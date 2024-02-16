751cd3bd058b5e21faac57238973a7d67dc9d56f
shashanksoul <whyshashank@gmail.com>
2024-02-16 21:26:43 +0530

    added jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/src/navigation/',
+    'src/index.tsx',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
 };
